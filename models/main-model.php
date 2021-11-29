b8aa49a360152e39c0a680f9d8cba86178607ff9
yannick <yannickkamdemkouam@yahoo.fr>
2021-11-29 23:00:30 +0100

    add to heroku

--- models/main-model.php
+++ models/main-model.php
@@ -7,10 +7,12 @@ trait MainModel
     public function __construct()
     {
         try {
-            $this->pdo = new PDO('mysql:host=localhost;dbname=boombox_city;charset=utf8', 'root', '');
+            // $this->pdo = new PDO('mysql:host=localhost;dbname=boombox_city;charset=utf8', 'root', '');
+            $this->pdo = new PDO('mysql:bd30b0362e7c98:30393616@us-cdbr-east-04.cleardb.com/heroku_7af5250ecc04f74?reconnect=true');
         }
         catch (PDOException $e) {
-            echo 'Un problème est survenue lors de la connection à la base de donnée';
+            echo $e;
+            exit();
         }
     }
 }
